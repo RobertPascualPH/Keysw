a398c1b1ae62dcede1bfd251819a4e36571f7d80
Robert Pascual <rtonypascual@gmail.com>
2016-06-14 14:16:38 +0800

    No comment.

--- keysw.h
+++ keysw.h
@@ -5,11 +5,32 @@
  *
  * ****************************************************/
 #include <QtGui>
+#include <QObject>
+#include <QPushButton>
+#include <QLabel>
 
-class KeySwitcher
+
+enum {
+    KS_English = 0, KS_Russian, num_languages
+};
+
+
+class KeySwitcher : public QObject
 {
     Q_OBJECT
 
-    
-}
+    public slots:
+        void switch_keyboard(void);
+
+    public:
+        KeySwitcher(QWidget *);   /* This constructor creates the dialog. */
+        ~KeySwitcher(void);
+
+    private:
+        int current_language;
+        QPushButton *KS_toggle_switch;
+        QLabel *KS_banner;
+        
+};
+
 
